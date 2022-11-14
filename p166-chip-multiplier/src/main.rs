68a3a51346f171210bb2b30ded17310061735cf5
andrei <andrei.reanu@gmail.com>
2022-11-14 22:27:38 +0200

    p166

---
+++ p166-chip-multiplier/src/main.rs
@@ -0,0 +1,11 @@
+// The basic difference between the register and memory is that 
+// the REGISTER holds the data that CPU is currently processing whereas, 
+// the MEMORY holds program instruction and data that the program requires for execution.
+
+struct CPU {
+    registers: [u8; 16],
+}
+
+fn main() {
+
+}
