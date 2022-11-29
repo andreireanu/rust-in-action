7e6aafbaa5247923b6d34896d6a29d8cc122db06
andrei <andrei.reanu@gmail.com>
2022-11-29 19:53:17 +0200

    p226 Match not working

--- p225-public-api/src/main.rs
+++ p225-public-api/src/main.rs
@@ -8,6 +8,26 @@ Usage:
     akv_mem.exe FILE update KEY VALUE
 ";
 
+#[cfg(not(target_os = "windows"))]
+const USAGE: &str = "
+Usage:
+    akv_mem FILE get KEY
+    akv_mem FILE delete KEY
+    akv_mem FILE insert KEY VALUE
+    akv_mem FILE update KEY VALUE
+";
+ 
+    fn main() {
+        let args: Vec<String> = std::env::args().collect();
+        let fname = args.get(1).expect(&USAGE);
+    
+        let action = args.get(2).expect(&USAGE).as_ref();
+        let key = args.get(3).expect(&USAGE).as_ref();
+        let maybe_value = args.get(4);
+        let path = std::path::Path::new(&fname);
+        let mut store = ActionKV::open(path).expect("unable to open file");
+        store.load().expect("unable to load data");
+    }
 
 match action {
     "get" => match store.get(key).unwrap() {
@@ -27,16 +47,4 @@ match action {
         store.update(key, value).unwrap()
     }
     _ => eprintln!("{}", &USAGE),
-    }
-
-    fn main() {
-        let args: Vec<String> = std::env::args().collect();
-        let fname = args.get(1).expect(&USAGE);
-    
-        let action = args.get(2).expect(&USAGE).as_ref();
-        let key = args.get(3).expect(&USAGE).as_ref();
-        let maybe_value = args.get(4);
-        let path = std::path::Path::new(&fname);
-        let mut store = ActionKV::open(path).expect("unable to open file");
-        store.load().expect("unable to load data");
     }
