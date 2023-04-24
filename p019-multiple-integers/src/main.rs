abc4b440726dfbbacdb3b686913ac6c8fb4197f4
andrei <andrei.reanu@gmail.com>
2023-04-24 21:37:55 +0300

    p033

--- p019-multiple-integers/src/main.rs
+++ p019-multiple-integers/src/main.rs
@@ -1,3 +1,10 @@
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+
 fn main() {
-    println!("Hello, world!");
+    let a = 10; // integer on Stack
+    let b = Box::new(20); // integer on Heap
+    let c = Rc::new(Box::new(30)); // Reference counter Boxed integer
+    let d = Arc::new(Mutex::new(40)); // Atomic reference counter
+    println!("a: {:?}, b: {:?}, c: {:?}, d: {:?}", a, b, c, d);
 }
