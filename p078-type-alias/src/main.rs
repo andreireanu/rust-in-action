68e481f410ae3b519a1ee4282235abf70054f705
andrei <andrei.reanu@gmail.com>
2023-07-12 14:25:07 +0300

    p085

--- p078-type-alias/src/main.rs
+++ p078-type-alias/src/main.rs
@@ -6,6 +6,12 @@ struct File {
     data: Vec<u8>,
 }
 
+impl File {
+    fn new(name:&str, data:Vec<u8>) -> Self {
+        Self { name: String::from(name), data }
+    }
+}
+
 fn open(f: &mut File) -> bool {
     true 
 }
@@ -45,12 +51,22 @@ fn main() {
     let f2_length = read(&f2, &mut buffer);
     close(&mut f2);
 
-    let text = String::from_utf8_lossy(&buffer);
+    let text2 = String::from_utf8_lossy(&buffer);
 
     println!("{:?}", f2);
     println!("{} is {} bytes long", &f2.name, f2_length);
-    println!("{}", text);
+    println!("{}", text2);
 
+    let mut f3 = File::new( "f3.txt", Vec::new());
+    f3.data.push(78);
+    f3.data.push(103);
+    f3.data.push(90);
+    buffer.clear();
+    let f3_length = read(&f3, &mut buffer);
+    let text3 = String::from_utf8_lossy(&buffer);
+    println!("{:?}", f3);
+    println!("{} is {} bytes long", &f3.name, f3_length);
+    println!("{}", text3);
 
 
 }
